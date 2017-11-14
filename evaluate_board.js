38843fb649f610831b24a5481fffacfeef005943
interimadd <interimadd@gmail.com>
2017-11-14 22:06:50 +0900

    局面評価をクラス化
    ポジションと解析時間を入力すると、
    局面評価と候補手探索を始めるクラス
    評価値の渡し方は検討中

--- evaluate_board.js
+++ evaluate_board.js
@@ -1,72 +1,52 @@
+
 var stockfish = require("stockfish/src/stockfish.js");
-var engine    = stockfish();
-
-var position = "fen rnbqkbnr/pp1ppppp/8/2p5/4P3/5N2/PPPP1PPP/RNBQKB1R b KQkq - 1 2"
 
 
-
-
-var got_uci;
-var started_thinking;
-
-
-/*
-// 
 var EvaluateBoard = function(FEN_format_position,evaluation_time_length) {
-    // メンバ変数 (インスタンス変数)
-    this.name = name;
-    this.age  = age;
-}
-*/
 
-function send(str)
-{
-    console.log("Sending: " + str)
-    engine.postMessage(str);
-}
+    // 結果を格納するメンバー変数
+    var self        = this;
+    this.best_move  = "";
 
-engine.onmessage = function(line) {
+    /* チェスエンジンを起動する */
+    this.engine    = stockfish();
+    this.engine.postMessage("uci");
 
-    console.log(line);
+    /////////////* ここまでコンストラクタ *////////////////
+    
+    /* 処理のメイン部分　エンジンから送られてくるメッセージを解析して処理する */
+    this.engine.onmessage = function(line) {
+    
+        this.send = function(str){
+            console.log("Sending: " + str)
+            this.postMessage(str);
+        };
 
-    /*
-    if(line.indexOf("Space")>-1)
-    {
-    	line = line.trimLeft();
-    	console.log("Space place:"+line.indexOf("Space"));
-    	console.log("Space length:"+line.length);
-    	moziretu = line.split(" ");
-    	console.log(moziretu[0]);
-    	console.log(moziretu[1]);
-    	console.log(moziretu[2]);
-    	console.log(moziretu[3]);
-    }
-    */
-
-    if (!got_uci && line === "uciok") {
-        got_uci = true;
-        if (position) {
-            send("position " + position);
-            send("eval");
-            send("d");
-        }
+        console.log(line);
         
-        //send("go ponder");
-    }
-    else if (!started_thinking && line.indexOf("info depth") > -1) {
-        console.log("Thinking...");
-        started_thinking = true;
-        setTimeout(function ()
+        // チェスエンジンの準備ができた通知が来たときの処理
+        if (line === "uciok") 
         {
-            send("stop");
-        }, 1000 * 2);
-    } else if (line.indexOf("bestmove") > -1) {
-        match = line.match(/bestmove\s+(\S+)/);
-        if (match) {
-            console.log("Best move: " + match[1]);
-            process.exit();
-        }
-    }
-};
+            this.send("position " + FEN_format_position);   // ポジションの設定
+            this.send("eval");                              // 局面評価開始
+            this.send("d");                                 // 盤面をAA表示
+            this.send("go ponder");                         // 候補手探索開始
 
-engine.postMessage("uci");
+            var f = this;
+            setTimeout( function (){f.send("stop");} , 1000 * evaluation_time_length); // 何秒後に候補手探索を打ち切るかを設定
+        }
+        // 候補手探索が打ち切られると通知される最善手に対する処理
+        else if (line.indexOf("bestmove") > -1) {
+            match = line.match(/bestmove\s+(\S+)/);
+            if (match) {
+                //console.log("Best move: " + match[1]);
+                self.best_move = match[1];
+                process.exit();
+            }
+        }
+    };
+
+}
+
+var position = "fen rnbqkbnr/pp1ppppp/8/2p5/4P3/5N2/PPPP1PPP/RNBQKB1R b KQkq - 1 2";
+var evaluator = new EvaluateBoard(position,2);
